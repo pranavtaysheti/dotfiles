8f9ab99e1810f714029a4faa1edd1cfa0515aa7f
Pranav Taysheti <pranavtaysheti@gmail,com>
2022-11-05 14:37:47 +0530

    05/11/2022

--- Scripts/passwords.sh
+++ Scripts/passwords.sh
@@ -1,2 +1,2 @@
-rclone mount GoogleDrive: ~/Google\ Drive --daemon
+rclone mount GoogleDrive: ~/Google\ Drive --vfs-cache-mode full --vfs-cache-max-age 360h --dir-cache-time 360h --vfs-cache-max-size 1024M --daemon
 secret-tool lookup database passwords | keepassxc --pw-stdin --keyfile ~/Certificates/passwords.keyx ~/Google\ Drive/Passwords/Passwords_web.kdbx &
