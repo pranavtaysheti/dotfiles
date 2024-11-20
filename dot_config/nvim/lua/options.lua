45ef95db16f699c947fb2467db60db0809578f7b
Pranav Taysheti <pranavtaysheti@gmail.com>
2024-11-20 15:04:27 +0530

    20/11/2024

---
+++ dot_config/nvim/lua/options.lua
@@ -0,0 +1,8 @@
+require "nvchad.options"
+
+-- add yours here!
+
+local o = vim.o
+o.tabstop=4
+o.shiftwidth=4
+-- o.cursorlineopt ='both' -- to enable cursorline!
---
+++ dot_config/nvim/lua/plugins/go.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    "ray-x/go.nvim",
+    dependencies = {  -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup({
+            goimports = 'gopls', -- if set to 'gopls' will use golsp format
+            gofmt = 'gopls', -- if set to gopls will use golsp format
+            lsp_cfg = true, -- false: use your own lspconfig
+            lsp_gofumpt = true, -- true: set default gofmt in gopls format to gofumpt
+            lsp_on_attach = true, -- use on_attach from go.nvim
+      })
+    end,
+    event = {"CmdlineEnter"},
+    ft = {"go", 'gomod'},
+    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+    init = function ()
+      local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          pattern = "*.go",
+          callback = function()
+          require('go.format').goimports()
+          end,
+          group = format_sync_grp,
+        })
+    end
+  },
+}
