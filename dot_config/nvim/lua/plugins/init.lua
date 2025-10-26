18fecd82049c5b64512a8eac56ac9bfdfca0947e
Pranav Taysheti <pranavtaysheti@gmail.com>
2025-10-26 19:58:58 +0000

    Update .config/nvim/init.lua Update .config/nvim/lua/chadrc.lua Update .config/nvim/lua/configs/lspconfig.lua Update .config/nvim/lua/plugins/init.lua

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -13,13 +13,16 @@ return {
     end,
   },
 
-  {
-  	"nvim-treesitter/nvim-treesitter",
-  	opts = {
-  		ensure_installed = {
-  			"vim", "lua", "vimdoc",
-       "html", "css"
-  		},
-  	},
-  },
+  -- test new blink
+  -- { import = "nvchad.blink.lazyspec" },
+
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
 }
